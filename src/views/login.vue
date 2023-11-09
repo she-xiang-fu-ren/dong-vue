5162bc9399eb7fae539d3b63492975c8887ece80
chen <85686468+1975947214@users.noreply.github.com>
2023-11-09 10:22:23 +0800

    测试111

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,58 +1,140 @@
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
-      <el-form-item prop="username">
-        <el-input
-          v-model="loginForm.username"
-          type="text"
-          auto-complete="off"
-          placeholder="账号"
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+    >
+      <div class="form-cont">
+        <el-tabs class="form" v-model="loginForm.loginType" style="float: none">
+          <el-tab-pane label="账号密码登录" value="uname">
+            <!-- 账号密码登录 -->
+            <el-form-item prop="username">
+              <el-input
+                v-model="loginForm.username"
+                type="text"
+                auto-complete="off"
+                placeholder="账号"
+              >
+                <svg-icon
+                  slot="prefix"
+                  icon-class="user"
+                  class="el-input__icon input-icon"
+                />
+              </el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input
+                v-model="loginForm.password"
+                type="password"
+                auto-complete="off"
+                placeholder="密码"
+                @keyup.enter.native="handleLogin"
+              >
+                <svg-icon
+                  slot="prefix"
+                  icon-class="password"
+                  class="el-input__icon input-icon"
+                />
+              </el-input>
+            </el-form-item>
+            <el-form-item v-if="captchaEnabled">
+              <el-input
+                v-model="loginForm.code"
+                auto-complete="off"
+                placeholder="验证码"
+                style="width: 63%"
+                @keyup.enter.native="handleLogin"
+              >
+                <svg-icon
+                  slot="prefix"
+                  icon-class="validCode"
+                  class="el-input__icon input-icon"
+                />
+              </el-input>
+              <div class="login-code">
+                <img :src="codeUrl" @click="getCode" class="login-code-img" />
+              </div>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="短信验证码登录" value="sms">
+            <!-- 短信验证码登录 -->
+            <el-form-item prop="mobile">
+              <el-input
+                v-model="loginForm.mobile"
+                type="text"
+                auto-complete="off"
+                placeholder="请输入手机号"
+              >
+                <svg-icon
+                  slot="prefix"
+                  icon-class="phone"
+                  class="el-input__icon input-icon"
+                />
+              </el-input>
+            </el-form-item>
+            <el-form-item prop="mobileCode">
+              <el-input
+                v-model="loginForm.mobileCode"
+                type="text"
+                auto-complete="off"
+                placeholder="短信验证码"
+                class="sms-login-mobile-code-prefix"
+                @keyup.enter.native="handleLogin"
+              >
+                <template>
+                  <svg-icon
+                    slot="prefix"
+                    icon-class="password"
+                    class="el-input__icon input-icon"
+                  />
+                </template>
+                <template slot="append">
+                  <span
+                    v-if="mobileCodeTimer <= 0"
+                    class="getMobileCode"
+                    @click="getSmsCode"
+                    style="cursor: pointer"
+                    >获取验证码</span
+                  >
+                  <span v-if="mobileCodeTimer > 0" class="getMobileCode"
+                    >{{ mobileCodeTimer }}秒后可重新获取</span
+                  >
+                </template>
+              </el-input>
+            </el-form-item>
+          </el-tab-pane>
+        </el-tabs>
+
+        <el-checkbox
+          v-model="loginForm.rememberMe"
+          style="margin: 0px 0px 25px 0px"
+          >记住密码</el-checkbox
         >
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
-          type="password"
-          auto-complete="off"
-          placeholder="密码"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="code" v-if="captchaEnabled">
-        <el-input
-          v-model="loginForm.code"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+        <el-form-item style="width: 100%">
+          <el-button
+            :loading="loading"
+            size="medium"
+            type="primary"
+            style="width: 100%"
+            @click.native.prevent="handleLogin"
+          >
+            <span v-if="!loading">登 录</span>
+            <span v-else>登 录 中...</span>
+          </el-button>
+          <div style="float: right" v-if="register">
+            <router-link class="link-type" :to="'/register'"
+              >立即注册</router-link
+            >
+          </div>
+        </el-form-item>
+        <div>
+          <el-button size="large" type="text" @click="unbind()"
+            >绑定钉钉</el-button
+          >
         </div>
-      </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
-        <el-button
-          :loading="loading"
-          size="medium"
-          type="primary"
-          style="width:100%;"
-          @click.native.prevent="handleLogin"
-        >
-          <span v-if="!loading">登 录</span>
-          <span v-else>登 录 中...</span>
-        </el-button>
-        <div style="float: right;" v-if="register">
-          <router-link class="link-type" :to="'/register'">立即注册</router-link>
-        </div>
-      </el-form-item>
+      </div>
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
@@ -62,55 +144,104 @@
 </template>
 
 <script>
-import { getCodeImg } from "@/api/login";
+import { getCodeImg,sendSmsCode,dinGing} from "@/api/login";
 import Cookies from "js-cookie";
-import { encrypt, decrypt } from '@/utils/jsencrypt'
+import { encrypt, decrypt } from "@/utils/jsencrypt";
+import {socialBind, socialUnbind} from "@/api/system/socialUser";
 
 export default {
   name: "Login",
   data() {
     return {
+      mobileCodeTimer: 0,
       codeUrl: "",
       loginForm: {
+        loginType: "uname",
         username: "admin",
-        password: "admin123",
+        password: "123456",
         rememberMe: false,
         code: "",
-        uuid: ""
+        uuid: "",
       },
       loginRules: {
         username: [
-          { required: true, trigger: "blur", message: "请输入您的账号" }
+          { required: true, trigger: "blur", message: "请输入您的账号" },
         ],
         password: [
-          { required: true, trigger: "blur", message: "请输入您的密码" }
+          { required: true, trigger: "blur", message: "请输入您的密码" },
         ],
-        code: [{ required: true, trigger: "change", message: "请输入验证码" }]
+        // code: [{ required: true, trigger: "change", message: "请输入验证码" }],
       },
       loading: false,
       // 验证码开关
       captchaEnabled: true,
       // 注册开关
       register: false,
-      redirect: undefined
+      redirect: undefined,
     };
   },
   watch: {
     $route: {
-      handler: function(route) {
+      handler: function (route) {
         this.redirect = route.query && route.query.redirect;
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   created() {
     this.getCode();
     this.getCookie();
+    // socialBind("", code, state).then(resp => {
+    //   this.$modal.msgSuccess("绑定成功");
+    //   this.$router.replace('/user/profile');
+    //   // 调用父组件, 刷新
+    //   this.getUser();
+    //   this.setActiveTab('userSocial');
+    // });
   },
   methods: {
+    unbind(){
+      let url="http://5ncxfk.natappfree.cc/system/dinging/auth";
+        dinGing(url).then((res)=>{
+          if(res.code==0){
+            window.location.href=res.data
+          }
+        })
+    },
+    bind() {
+      // 计算 redirectUri
+      const redirectUri =  'http://vxw7qt.natappfree.cc'+ '/user/profile';
+      // 进行跳转
+      socialAuthRedirect( encodeURIComponent(redirectUri)).then((res) => {
+        // console.log(res.url);
+        window.location.href = res.data;
+      });
+    },
+    getSmsCode() {
+      if (this.mobileCodeTimer > 0) return;
+      this.$refs.loginForm.validate((valid) => {
+        if (!valid) return;
+        sendSmsCode(
+          {phone:this.loginForm.mobile,}
+          // this.scene,
+          // this.loginForm.uuid,
+          // this.loginForm.code
+        ).then((res) => {
+          this.$modal.msgSuccess("获取验证码成功");
+          this.mobileCodeTimer = 60;
+          let msgTimer = setInterval(() => {
+            this.mobileCodeTimer = this.mobileCodeTimer - 1;
+            if (this.mobileCodeTimer <= 0) {
+              clearInterval(msgTimer);
+            }
+          }, 1000);
+        });
+      });
+    },
     getCode() {
-      getCodeImg().then(res => {
-        this.captchaEnabled = res.captchaEnabled === undefined ? true : res.captchaEnabled;
+      getCodeImg().then((res) => {
+        this.captchaEnabled =
+          res.captchaEnabled === undefined ? true : res.captchaEnabled;
         if (this.captchaEnabled) {
           this.codeUrl = "data:image/gif;base64," + res.img;
           this.loginForm.uuid = res.uuid;
@@ -120,38 +251,46 @@ export default {
     getCookie() {
       const username = Cookies.get("username");
       const password = Cookies.get("password");
-      const rememberMe = Cookies.get('rememberMe')
+      const rememberMe = Cookies.get("rememberMe");
       this.loginForm = {
         username: username === undefined ? this.loginForm.username : username,
-        password: password === undefined ? this.loginForm.password : decrypt(password),
-        rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
+        password:
+          password === undefined ? this.loginForm.password : decrypt(password),
+        rememberMe: rememberMe === undefined ? false : Boolean(rememberMe),
       };
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true;
           if (this.loginForm.rememberMe) {
             Cookies.set("username", this.loginForm.username, { expires: 30 });
-            Cookies.set("password", encrypt(this.loginForm.password), { expires: 30 });
-            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 });
+            Cookies.set("password", encrypt(this.loginForm.password), {
+              expires: 30,
+            });
+            Cookies.set("rememberMe", this.loginForm.rememberMe, {
+              expires: 30,
+            });
           } else {
             Cookies.remove("username");
             Cookies.remove("password");
-            Cookies.remove('rememberMe');
+            Cookies.remove("rememberMe");
           }
-          this.$store.dispatch("Login", this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || "/" }).catch(()=>{});
-          }).catch(() => {
-            this.loading = false;
-            if (this.captchaEnabled) {
-              this.getCode();
-            }
-          });
+          this.$store
+            .dispatch("Login", this.loginForm)
+            .then(() => {
+              this.$router.push({ path: this.redirect || "/" }).catch(() => {});
+            })
+            .catch(() => {
+              this.loading = false;
+              if (this.captchaEnabled) {
+                this.getCode();
+              }
+            });
         }
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -164,12 +303,16 @@ export default {
   background-image: url("../assets/images/login-background.jpg");
   background-size: cover;
 }
-.title {
+// .title {
+//   margin: 0px auto 30px auto;
+//   text-align: center;
+//   color: #707070;
+// }
+.form-cont {
   margin: 0px auto 30px auto;
   text-align: center;
   color: #707070;
 }
-
 .login-form {
   border-radius: 6px;
   background: #ffffff;
